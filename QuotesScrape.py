a1263626ffd37ff2d7ef73f1c1dc3dc3cfa9f930
Pankaj-Baranwal <pankajisadeveloper@gmail.com>
2018-10-02 03:16:51 +0530

    Script now follows PEP-8 guidelines
    Rearranged code, added comments, followed PEP-8 guidelines

--- QuotesScrape.py
+++ QuotesScrape.py
@@ -1,38 +1,44 @@
-from bs4 import BeautifulSoup
-import requests
-import sys
 import csv
-import re
 
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://www.brainyquote.com/topics/love'
+
+# Open a csv file in write mode
 csv_file = open('quotes.csv', 'w')
 
 csv_writer = csv.writer(csv_file)
-csv_writer.writerow(['Quotes','Author'])
+# Add Heading as first row
+csv_writer.writerow(['Quotes', 'Author'])
 
-
-code = requests.get('https://www.brainyquote.com/topics/love')
+code = requests.get(URL)
 soup = BeautifulSoup(code.text, 'html.parser')
 
-for para in soup.find_all('div',class_='m-brick grid-item boxy bqQt'):
-	#quotes 
-	quotes = para.find('div',class_="clearfix")
+# Find all elements with given classes
+for para in soup.find_all('div', class_='m-brick grid-item boxy bqQt'):
+    # Extracted Quotes
+    quotes = para.find('div', class_="clearfix")
 
-	_a_list = quotes.find_all('a')
-	try:	
+    # Find all <a> tags in the body of webpage
+    _a_list = quotes.find_all('a')
+    try:
+        # Extract link of anchor tag of quote
+        apply_link = _a_list[1].text
+        print(apply_link)
 
-		apply_link = _a_list[1].text
-		print(apply_link)
+        # Extract link of anchor tag of author
+        web_link = _a_list[2].text
+        print(web_link)
 
-		web_link = _a_list[2].text
-		print(web_link)
-	
-	except IndexError as m:
-		apply_link = _a_list[0].text
-		print(apply_link)
+    except IndexError as m:
+        apply_link = _a_list[0].text
+        print(apply_link)
 
-		web_link = _a_list[1].text
-		print(web_link)
+        web_link = _a_list[1].text
+        print(web_link)
 
-	csv_writer.writerow([apply_link,web_link])
+    # Write row to csv file
+    csv_writer.writerow([apply_link, web_link])
 
-csv_file.close
+csv_file.close()
